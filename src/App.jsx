26457e522f2f1fe79ad3c75f2e9e19a1fde391ba
MidhunChandera <kmidhun666@gmail.com>
2025-02-07 13:43:36 +0530

    Fixed import path issue in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,12 @@ import Header from "./components/Header"
 
 import Auth from "../pages/Auth"
 import Createpost from "../pages/Createpost"
-
-import Postdetails from "../pages/postdetails"
 import Myblog from "../pages/Myblog"
 import About from "../pages/About"
 
 import { ToastContainer } from 'react-toastify'; // Import ToastContainer
 import 'react-toastify/dist/ReactToastify.css';
+import PostDetails from "../pages/postdetails"
 function App() {
 
 
@@ -26,7 +25,7 @@ function App() {
         <Route path='/create' element={<Createpost />} />
         <Route path='/myblogs' element={<Myblog />} />
         <Route path='/about' element={<About />} />
-        <Route path="/postdetails/:blogid" element={<Postdetails />} />
+        <Route path="/postdetails/:blogid" element={<PostDetails />} />
         <Route path='/login' element={<Auth register={false} />} />
         <Route path='/register' element={<Auth register={true} />} />
       </Routes>
